bec46eb222587e589a31b17b838ffc482b746136
AhmedAdelMobDeveloper <AhmedAdelAD@hotmail.com>
2018-05-06 18:27:22 +0200

    - login screen and signup screen

--- app/src/main/java/com/shefaa/LoginActivity.java
+++ app/src/main/java/com/shefaa/LoginActivity.java
@@ -1,13 +1,16 @@
 package com.shefaa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText editID, editPassword;
+    private Button loginButt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     private void initUi() {
         editID = (EditText) findViewById(R.id.edit_login_screen_id);
         editPassword = (EditText) findViewById(R.id.edit_login_screen_password);
+        loginButt = (Button) findViewById(R.id.login_butt);
 
         editID.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -31,6 +35,14 @@ public class LoginActivity extends AppCompatActivity {
                 } else {
                     editID.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
                 }
+            }
+        });
+
+        loginButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
             }
         });
     }
